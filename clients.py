f7bd9551882c5fce8ee68cd075e492ca99ce5339
kgeor <34303609+kgeor@users.noreply.github.com>
2019-04-23 20:04:06 +0300

    Update clients.py

--- clients.py
+++ clients.py
@@ -19,8 +19,42 @@ class pkt:
   while (src[1]==dst[1])&(src[2]==dst[2]):
     dst=random.choice(list(cam.keys()))
 
-a=pkt()    
-print(a.src)
-print(a.dst)
+P=pkt()    
+print(p.src)
+print(p.dst)
 print(a.ports)
-  
+
+CAM и Port словари сделать глобальными. Возможно инициализировать их в спец классе build
+Класс switchport перевести в buffer, новый класс switcport  имеет два атрибута, равные экземплярам класса buffer
+Сделать класс switch, который соответственно будет сопоставлять порт входа.in с портом выхода.out:
+Class params:
+PORTS=10
+RATE=
+BUF=
+DISTRIB
+
+Class clients:
+Def init(self, num, ports) 
+Num, ports - равноразмерные списки
+
+Class switchport(rate, bufsize):
+Self.in=None #буфер приходящих извне пакетов
+Self.out=None #буфер уходящих с коммутатора
+
+Class switch:
+Def init(self, pkt) 
+
+i=p.cam[pkt.dst]
+SwitchPort[i-1].out=pkt
+#мб добавить маааленькую рандомную задержку
+
+
+Вообще порядок:
+Опред кво портов и их парам(с б) 
+Опред кво клиентов и их парам(ид) 
+Подсоединить клиентов к портам
+Составить таблицы 
+Инициализировать коммутатор, передав ему таблицы
+
+Надо бы сделать так, чтобы в один момент пакеты отправляло опред число клиентов. 
+В pgen для каждого клиента дать свое распределение времени отправки
